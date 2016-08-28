482abf48226a9d3a61a1d6471467ad4344e1a387
Hayden Flinner <hayden@gatech.edu>
2016-08-28 14:08:32 -0400

    Works! Fix URL matching

--- content.js
+++ content.js
@@ -1,12 +1,16 @@
-setInterval(function hidePosts() {
-    var divs = document.getElementById("contentArea").getElementsByTagName('div');
-    var elements = document.querySelectorAll('[data-testid^=fbfeed_story]');
-    for (var i = 0; i < elements.length; i++) {
-        var x = elements[i].querySelectorAll('[class^=uiStreamSponsoredLink]'); // Sponsored links
-        if (x.length > 0) {
-            elements[i].parentNode.removeChild(elements[i]);
-            console.log('Removed: ');
-            console.log(elements[i]);
-        }
+console.log("Executing...");
+var removeIfSalePost = function(node) {
+    if (node.querySelectorAll && node.querySelectorAll('[class^=_l52]').length > 0) {
+        node.parentNode.removeChild(node);
     }
-}, 2000);
+}
+document.querySelectorAll('[data-testid^=fbfeed_story]').forEach(function(node) { removeIfSalePost(node); });
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        mutation.addedNodes.forEach(function(added) {
+            removeIfSalePost(added);
+        });
+    });
+});
+observer.observe(document.querySelector('[id^=more_pager_pagelet_]'),
+        { attributes: true, childList: true, characterData: true, subtree: true });

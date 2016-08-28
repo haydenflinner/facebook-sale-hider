080a8cf851e866fb2bb90121184a841c60c29f51
Hayden Flinner <hayden@gatech.edu>
2016-08-28 16:48:48 -0400

    Added comments, cleaned up code and observer properties, fixed posts not being removed if you had notifications.

--- content.js
+++ content.js
@@ -1,36 +1,38 @@
+/** Uses the presence of the bold item title field in most sale posts to determine if sale post or not Check for 'Sale' in group name if this doesn't work well enough **/
 var removeIfSalePost = function(node) {
-    if (node.querySelectorAll && node.parentNode && node.querySelectorAll('[class^=_l52]').length > 0) {
-        node.parentNode.removeChild(node);
-    }
+    if (node.querySelectorAll && node.querySelectorAll('[class^=_l52]').length > 0) node.parentNode.removeChild(node);
 }
+
+/** Attaches to newsfeed, removes incoming saleposts before they are scrolled into view **/
 var observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
-	mutation.addedNodes.forEach(function(added) {
-	    removeIfSalePost(added);
-	});
+        mutation.addedNodes.forEach(function(added) {
+            removeIfSalePost(added);
+        });
     });
 });
+
+/** Removes sale group posts from the main DOM, then attaches and purges incoming sale posts **/
 var attachRemover = function() {
-	if (observer.connected) return;
-	observer.connected = true;
-	var possible = document.querySelector('[id^=topnews_main_stream]');
-	document.querySelectorAll('[data-testid^=fbfeed_story]').forEach(function(node) { removeIfSalePost(node); });
-	observer.observe(possible,
-		{ attributes: true, childList: true, characterData: true, subtree: true });
+    if (observer.connected) return;
+    observer.connected = true;
+    var posts = document.querySelectorAll('[data-testid^=fbfeed_story]');
+    posts.forEach(function(node) { removeIfSalePost(node); }); // Clear the posts loaded when page loads
+    var newsFeed = document.querySelector('[id^=topnews_main_stream]'); // Then remove them as they come into view
+    observer.observe(newsFeed, { childList: true, subtree: true });
 }
 
 var disconnectRemover = function() {
-	observer.connected = false;
-	observer.disconnect();
+    observer.connected = false;
+    observer.disconnect();
 }
 
+/** Called when tab title changes. Attaches sale post remover or disconnects it if leaving main feed. **/
 var pageWatcher = new window.MutationObserver(
-	function(mutations) {
-		mutations.forEach(
-			function(mutation) {
-				'Facebook' === mutation.target.textContent ? setTimeout(attachRemover, 500) : disconnectRemover();
-				}
-			});
-	});
-var target = document.querySelector('head > title');
-pageWatcher.observe(target, { subtree: true, characterData: true, childList: true });
+    function(mutations) {
+        mutations.forEach(function(mutation) {
+            mutation.target.textContent.indexOf('Facebook') != -1 ? setTimeout(attachRemover, 500) : disconnectRemover();
+        });
+    });
+var tabTitleText = document.querySelector('head > title');
+pageWatcher.observe(tabTitleText, { subtree: true, characterData: true, childList: true });
